5e9183219fa1098d316d9996e162c95ce5f02870
gima <gima@iki.fi>
2017-05-08 23:03:48 +0300

    Make Nil Validator use the new data access method

--- v1/nil.go
+++ v1/nil.go
@@ -2,31 +2,22 @@ package govalid
 
 import (
 	"fmt"
-	"reflect"
+
+	"github.com/gima/govalid/v1/internal"
 )
 
-// Validates data types to be nil
+// Constructs a validator which validates data type to be a `nil`.
 func Nil() Validator {
 	return &nilValidator{}
 }
 
-// validator for a nil value
 type nilValidator struct {
 }
 
-// the workhorse for the nil validator
 func (r *nilValidator) Validate(data interface{}) (string, error) {
-	value := reflect.ValueOf(data)
-
-	switch value.Kind() {
-	case reflect.Invalid:
+	if value := internal.DigValue(data); !value.IsValid() {
 		return "", nil
-	case reflect.Ptr:
-		if !value.IsNil() {
-			return "Nil", fmt.Errorf("expected <nil>, was %v", reflect.TypeOf(data))
-		}
-		return "", nil
-	default:
-		return "Nil", fmt.Errorf("expected <nil>, was %v", reflect.TypeOf(data))
+	} else {
+		return "Nil", fmt.Errorf("expected <nil>, was %v", value.Type())
 	}
 }
--- v1/tests/nil_test.go
+++ v1/tests/nil_test.go
@@ -1,18 +1,12 @@
 package govalid_test
 
 import (
-	v "github.com/gima/govalid/v1"
 	"testing"
+
+	v "github.com/gima/govalid/v1"
 )
 
 func TestNil(t *testing.T) {
-	var np *int = nil
-	var nnp int = 3
-
-	test(t, "nil value", true, v.Nil(), nil)
-	test(t, "non-string", false, v.Nil(), 3)
-	test(t, "string", false, v.Nil(), "cat's aren't nil")
-	test(t, "empty object", true, v.Nil(), interface{}(nil))
-	test(t, "pointer datatype", true, v.Nil(), np)
-	test(t, "int pointer", false, v.Nil(), &nnp)
+	test(t, "nil", true, v.Nil(), nil)
+	test(t, "non-nil", false, v.Nil(), "")
 }
